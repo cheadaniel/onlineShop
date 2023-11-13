cee93f51326ace70288154d7a17f9abdf2bfbc1c
Daniel.C <cheadaniel.95@gmail.com>
2023-11-13 04:18:40 +0100

    ajout de strore.js cartSlice et modif du index.js pour qu'il convienne au store
    cartSlice permet d'obtenir le json pour la requete de l'api : api/commands/create
    et avec les reducers on pourra gerer les quantité souhaité depuis la page du panier

---
+++ frontend/src/app/features/cart/cartSlice.js
@@ -0,0 +1,51 @@
+// cartSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        user_id: null, // Initialiser à null jusqu'à ce que l'utilisateur se connecte
+        status: 'En cours de préparation',
+        products: [],
+        total_price: '0.00',
+    },
+    reducers: {
+        setUserId: (state, action) => {
+            state.user_id = action.payload;
+        },
+        addToCart: (state, action) => {
+            const { product_id, quantity, name, product_price } = action.payload;
+            const existingProduct = state.products.find((product) => product.product_id === product_id);
+
+            if (existingProduct) {
+                existingProduct.quantity += quantity;
+                existingProduct.total_price = (parseFloat(existingProduct.total_price) + parseFloat(product_price) * quantity).toFixed(2);
+            } else {
+                state.products.push({
+                    product_id,
+                    quantity,
+                    total_price: (parseFloat(product_price) * quantity).toFixed(2),
+                    name,
+                    product_price,
+                });
+            }
+
+            // Recalculer le total_price en fonction des produits actuels dans le panier
+            state.total_price = state.products.reduce((total, product) => total + parseFloat(product.total_price), 0).toFixed(2);
+        },
+        removeFromCart: (state, action) => {
+            const productId = action.payload;
+            const removedProductIndex = state.products.findIndex(product => product.product_id === productId);
+
+            if (removedProductIndex !== -1) {
+                const removedProduct = state.products[removedProductIndex];
+                state.total_price = (parseFloat(state.total_price) - parseFloat(removedProduct.total_price)).toFixed(2);
+
+                state.products.splice(removedProductIndex, 1);
+            }
+        },
+    },
+});
+
+export const { setUserId, addToCart, removeFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
